b3c354e2ddf892aeec530bd75ab8f378fd381a07
MydroX <maxime@zinutti.fr>
2022-08-15 04:48:45 +0200

    fix error in env loading

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -14,13 +14,11 @@ func Load() {
 		if err != nil {
 			log.Fatal("Error loading .env file")
 		}
-		return
 	} else if os.Getenv("ENV") == "prod" {
 		err := godotenv.Load(".env")
 		if err != nil {
 			log.Fatal("Error loading .env file")
 		}
-		return
 	}
 
 	err = godotenv.Load(".env.local.db")
@@ -29,12 +27,10 @@ func Load() {
 		if err != nil {
 			log.Fatal("Error loading .env.db file")
 		}
-		return
 	} else if os.Getenv("ENV") == "prod" {
 		err := godotenv.Load(".env.db")
 		if err != nil {
 			log.Fatal("Error loading .env.db file")
 		}
-		return
 	}
 }
