e00cc28537ab94f3f8524cafc1a621c71b5303c0
MydroX <maxime@zinutti.fr>
2022-08-15 04:29:23 +0200

    small tweaks on env file loading

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -13,14 +14,27 @@ func Load() {
 		if err != nil {
 			log.Fatal("Error loading .env file")
 		}
+		return
+	} else if os.Getenv("ENV") == "prod" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+		return
 	}
 
 	err = godotenv.Load(".env.local.db")
-	if err != nil {
+	if err != nil || os.Getenv("ENV") == "prod" {
 		err := godotenv.Load(".env.db")
 		if err != nil {
 			log.Fatal("Error loading .env.db file")
 		}
+		return
+	} else if os.Getenv("ENV") == "prod" {
+		err := godotenv.Load(".env.db")
+		if err != nil {
+			log.Fatal("Error loading .env.db file")
+		}
+		return
 	}
-
 }
